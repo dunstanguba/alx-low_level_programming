fe7183662afd9250c43c9d503ccc69e6f93a7efe
dunstanguba <dunstan.guba@police.gov.gh>
2023-02-16 14:07:13 -0800

    Hello, printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 
 /*
+* main - main is the block of the code
 * Description: using the main function
 *  program output  "Programming is like building a multilingual puzzle
 * Return: 0
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - is the main block of the code
+* Description: using the main function
+* Program output is "with proper grammar, but the outcome is a piece of art,,
+* Return: 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
