c36daa386b76bd4459fd5da1a8ca3e7b443db6db
dunstanguba <dunstan.guba@police.gov.gh>
2023-02-16 14:16:07 -0800

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,12 @@
-
+lines (10 sloc)  154 Bytes
 
 #include <stdio.h>
-
-/*
-* main - main is the block of the code
-* Description: using the main function
-*  program output  "Programming is like building a multilingual puzzle
-* Return: 0
-*/
+/**
+ * main - main block
+ * Return: 0
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
