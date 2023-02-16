155012fb31427b2655802448d8fa1496bc1556a9
dunstanguba <dunstan.guba@police.gov.gh>
2023-02-16 13:59:35 -0800

    first C program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+
+
+#include <stdio.h>
+
+/*
+* Description: using the main function
+*  program output  "Programming is like building a multilingual puzzle
+* Return: 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
